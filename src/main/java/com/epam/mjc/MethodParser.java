097a0998f21d4204d8ec6a3b15930ed5bc331e34
Welkus <vdelkus@gmail.com>
2023-10-31 20:18:37 +0200

    TD

--- src/main/java/com/epam/mjc/MethodParser.java
+++ src/main/java/com/epam/mjc/MethodParser.java
@@ -1,25 +1,70 @@
 package com.epam.mjc;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class MethodParser {
 
     /**
      * Parses string that represents a method signature and stores all it's members into a {@link MethodSignature} object.
      * signatureString is a java-like method signature with following parts:
-     *      1. access modifier - optional, followed by space: ' '
-     *      2. return type - followed by space: ' '
-     *      3. method name
-     *      4. arguments - surrounded with braces: '()' and separated by commas: ','
+     * 1. access modifier - optional, followed by space: ' '
+     * 2. return type - followed by space: ' '
+     * 3. method name
+     * 4. arguments - surrounded with braces: '()' and separated by commas: ','
      * Each argument consists of argument type and argument name, separated by space: ' '.
      * Examples:
-     *      accessModifier returnType methodName(argumentType1 argumentName1, argumentType2 argumentName2)
-     *      private void log(String value)
-     *      Vector3 distort(int x, int y, int z, float magnitude)
-     *      public DateTime getCurrentDateTime()
+     * accessModifier returnType methodName(argumentType1 argumentName1, argumentType2 argumentName2)
+     * private void log(String value)
+     * Vector3 distort(int x, int y, int z, float magnitude)
+     * public DateTime getCurrentDateTime()
      *
      * @param signatureString source string to parse
      * @return {@link MethodSignature} object filled with parsed values from source string
      */
     public MethodSignature parseFunction(String signatureString) {
-        throw new UnsupportedOperationException("You should implement this method.");
+        List<MethodSignature.Argument> list = new ArrayList<>();
+
+        Pattern pattern = Pattern.compile("^(\\w+ )?(\\w+ )?(\\w+)\\(([^)]*)\\)");
+        Matcher matcher = pattern.matcher(signatureString);
+
+        if (!matcher.find()) {
+            throw new IllegalArgumentException("Invalid method signature");
+        }
+
+        String accessModifier = null;
+        String returnType;
+        String name;
+
+        if (matcher.group(2) == null) {
+            returnType = matcher.group(1);
+            name = matcher.group(3);
+        } else {
+            accessModifier = matcher.group(1).trim();
+            returnType = matcher.group(2).trim();
+            name = matcher.group(3);
+        }
+
+        String[] signatureParameters = matcher.group(4).trim().split(", ");
+
+        for (int i = 0; i < signatureParameters.length; i++) {
+            String[] argPair = signatureParameters[i].split(" ");
+            if (argPair.length == 2) {
+                list.add(new MethodSignature.Argument(argPair[0], argPair[1]));
+            }
+        }
+
+        MethodSignature ms = new MethodSignature(name, list);
+
+        if (accessModifier != null) {
+            ms.setAccessModifier(accessModifier.trim());
+        }
+        if (returnType != null) {
+            ms.setReturnType(returnType.trim());
+        }
+
+        return ms;
     }
 }
--- src/main/java/com/epam/mjc/StringSplitter.java
+++ src/main/java/com/epam/mjc/StringSplitter.java
@@ -1,7 +1,9 @@
 package com.epam.mjc;
 
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
+import java.util.regex.Pattern;
 
 public class StringSplitter {
 
@@ -13,6 +15,32 @@ public class StringSplitter {
      * @return List of substrings
      */
     public List<String> splitByDelimiters(String source, Collection<String> delimiters) {
-        throw new UnsupportedOperationException("You should implement this method.");
+
+        if(source == null || delimiters == null){
+            throw new UnsupportedOperationException("You should implement this method.");
+
+        }
+        List<String> list = new ArrayList<>();
+
+        StringBuilder regexBuilder = new StringBuilder();
+
+        for (String delimiter : delimiters){
+            if(regexBuilder.length() > 0){
+                regexBuilder.append("|");
+            }
+            regexBuilder.append(delimiter);
+        }
+
+        String regex = regexBuilder.toString();
+
+        String [] done = source.split(regex);
+
+        for (String s : done){
+           if(!s.isEmpty()) list.add(s);
+
+        }
+
+        return  list;
+
     }
 }
